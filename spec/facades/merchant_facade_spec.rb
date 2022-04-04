651818ade62822bce442200978b40ba4db8224b5
Conor Barthel <conor.barthel@gmail.com>
2022-04-04 13:58:41 -0600

    Feat: Merchant facade

--- spec/facades/merchant_facade_spec.rb
+++ spec/facades/merchant_facade_spec.rb
@@ -1,11 +1,10 @@
-equire 'rails_helper'
+require 'rails_helper'
 
 RSpec.describe MerchantFacade do
   describe '#get_merchants' do
     it 'gets all merchants' do
       VCR.use_cassette('merchants_api') do
         merchants = MerchantFacade.get_merchants
-
         merchants.each do |merchant|
           expect(merchant).to be_an_instance_of(MerchantCall)
         end
